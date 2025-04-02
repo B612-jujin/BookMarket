b0129eaea870ee537af6cb1621f4418ce8023ad7
B612-jujin <jujingood@gmail.com>
2025-04-02 11:41:05 +0900

    BookMaket Mapping 설정, 다양한 매핑 방식 시도

--- src/main/java/kr/ac/kopo/cjj/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/cjj/bookmarket/controller/BookController.java
@@ -6,20 +6,34 @@ import kr.ac.kopo.cjj.bookmarket.serveice.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
+@RequestMapping(value = "/books")
 public class BookController {
     @Autowired
     private BookService bookService; // 서비스계층과 연결
 
-    @RequestMapping(value = "/books",method = RequestMethod.GET) //페이지 주소가 호출되었을 때 발동!
-    public String requstBooklist(Model model){
+    @GetMapping
+    public String requstBookList(Model model) {
         List<Book> bookList = bookService.getAllBookList();
         model.addAttribute("bookList", bookList); // 모델에  bookList란  책 정보 전달
         return "books";
+    }
+
+
+    @RequestMapping(value = "/all",method = RequestMethod.GET) //페이지 주소가 호출되었을 때 발동!
+    public ModelAndView requstAllBooklist(){
+        ModelAndView modelView = new ModelAndView();
+        modelView.setViewName("books");
+        List<Book> bookList = bookService.getAllBookList();
+        modelView.addObject("bookList", bookList); // 모델에  bookList란  책 정보 전달
+
+        return modelView;
     }
 }
